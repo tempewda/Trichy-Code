43b9ee569ef1568e413be8296daf63a962be639e
Tanmoy <89120102+tempewda@users.noreply.github.com>
2024-09-04 10:35:48 +0530

    Update and rename test.py to P1_circle.py

---
+++ Sem 1/Python/Day 1/P1_circle.py
@@ -0,0 +1,9 @@
+# Write a python program to compute the area & perimeter of a circle
+
+radius = eval (input ("Enter the radius of the circle: "))
+
+area = 3.1412 * (radius ** 2)
+perimeter = 2 * 3.1412 * radius
+
+print ("Area of a circle is: ", area)
+print ("Perimeter of a circle is: ", perimeter)
