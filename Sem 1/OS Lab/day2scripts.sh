7569c4a07f8773865660369d9b09ac77510e9aa9
Tanmoy <89120102+tempewda@users.noreply.github.com>
2024-09-06 08:53:24 +0530

    Create day2scripts.sh

---
+++ Sem 1/OS Lab/day2scripts.sh
@@ -0,0 +1,27 @@
+205124100@labca-026:~$ nano script1.sh
+205124100@labca-026:~$ 205124100@labca-026:~$ cat script1.sh
+#! /bin/sh
+#First Script
+
+echo "Today's system date and time: "
+date
+echo "My Present working directory: "
+pwd
+echo "Current month's Calendar: "
+cal
+echo "Script ends here"
+205124100@labca-026:~$ sh script1.sh
+Today's system date and time:
+Friday 06 September 2024 08:50:05 AM IST
+My Present working directory:
+/home/Students/MCA_2024/205124100
+Current month's Calendar:
+   September 2024
+Su Mo Tu We Th Fr Sa
+ 1  2  3  4  5  6  7
+ 8  9 10 11 12 13 14
+15 16 17 18 19 20 21
+22 23 24 25 26 27 28
+29 30
+
+Script ends here
