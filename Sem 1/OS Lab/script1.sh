327c1e45e7ef7e651f88bdaff81afb92a5ca382c
Tanmoy <89120102+tempewda@users.noreply.github.com>
2024-09-06 08:53:07 +0530

    Create script1.sh

---
+++ Sem 1/OS Lab/script1.sh
@@ -0,0 +1,10 @@
+#! /bin/sh
+#First Script
+
+echo "Today's system date and time: "
+date
+echo "My Present working directory: "
+pwd
+echo "Current month's Calendar: "
+cal
+echo "Script ends here"
