422a9e856c66571ba0de972b7e46801e7ba39ff1
Tanmoy <89120102+tempewda@users.noreply.github.com>
2024-10-09 09:54:46 +0530

    Add files via upload

--- Sem 1/DS in C/CLBRTY.C
+++ Sem 1/DS in C/CLBRTY.C
@@ -1,45 +1,51 @@
 # include <stdio.h>
 # include <stdlib.h>
+# include <conio.h>
 
-int find_celebrity(int size)
+int find_celebrity(const int size)
 {
-    int i = 0, j = 0, candidate = -1; 
-    int matrix[size][size];
-    int stack[size];
+    int i = 0, j = 0, candidate = -1;
     int top = -1;
+    int **matrix = (int**)malloc(size * sizeof(int*));
+    int *stack = (int*)malloc(size * sizeof(int));
+    for (i = 0; i < size; ++i)
+	matrix[i] = (int*)malloc(size * sizeof(int));
+    //int matrix[size][size];
+
+
 
     // input values in the matrix
     printf("Enter values into the matrix:\n");
-    for (i = 0; i < size; ++i) 
-        for (j = 0; j < size; ++j) 
-            scanf("%d", &matrix[i][j]);
+    for (i = 0; i < size; ++i)
+	for (j = 0; j < size; ++j)
+	    scanf("%d", &matrix[i][j]);
 
     // intialize the stack
     for (top = 0; top < size; ++top)
-        stack[top] = top;
+	stack[top] = top;
     --top;
 
     while (top > 0) {
-        // pop 2 items
-        i = stack[top--];
-        j = stack[top--];
+	// pop 2 items
+	i = stack[top--];
+	j = stack[top--];
 
-        if (matrix[i][j] == 1 && matrix[j][i] == 0)
-            stack[++top] = j;
+	if (matrix[i][j] == 1 && matrix[j][i] == 0)
+	    stack[++top] = j;
 
-        else if (matrix[j][i] == 1 && matrix[i][j] == 0)
-            stack[++top] = i;
+	else if (matrix[j][i] == 1 && matrix[i][j] == 0)
+	    stack[++top] = i;
     }
     if (top == -1)
-        return -1;
-    
+	return -1;
+
     // verify that candidate is celebrity indeed
     candidate = stack[top];
 
     for (i = 0; i < size; ++i)
-        if (i != candidate)
-            if (matrix[candidate][i] == 1 || matrix[i][candidate] == 0)
-                return -1;
+	if (i != candidate)
+	    if (matrix[candidate][i] == 1 || matrix[i][candidate] == 0)
+		return -1;
 
     // all tests passed
     return candidate;
@@ -55,9 +61,11 @@ int main()
 
     celebrity = find_celebrity(people);
     if (celebrity == -1)
-        printf("\nNo Celebrity found!!\n");
+	printf("\nNo Celebrity found!!\n");
     else
-        printf("\nCelebrity: %d\n", celebrity);
- 
+	printf("\nCelebrity: %d\n", celebrity);
+
+    getch();
+    clrscr();
     return 0;
 }
--- Sem 1/DS in C/RVRSSTR.C
+++ Sem 1/DS in C/RVRSSTR.C
@@ -1,4 +1,5 @@
 # include <stdio.h>
+# include <conio.h>
 
 int main()
 {
@@ -7,20 +8,21 @@ int main()
     int top = -1; // denotes empty stack
 
     printf("Input string: ");
-    
-    // Enter the characaters individually until \n is hit
+
+    // Enter the characters individually until \n is hit
     scanf("%c", &ch);
     while (ch != '\n') {
-        str[++top] = ch;
-        scanf("%c", &ch);
+	str[++top] = ch;
+	scanf("%c", &ch);
     }
     str[top + 1] = '\0';
 
     // Reverse the string using pop opertion
     printf("Reversed String: ");
     while (top >= 0)
-        printf("%c", str[top--]);
+	printf("%c", str[top--]);
     printf("\n");
-
+    getch();
+    clrscr();
     return 0;
 }
